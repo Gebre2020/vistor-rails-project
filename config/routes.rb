730bc91dec24598ed8cbec8fd0bb5036b9315dcc
Gebremeskel <gebre20122@gmail.com>
2021-05-07 18:34:33 -0500

    adding root and travel routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
+  root "travels#index"
+
+  # get "/travels", to: "travels#index"
+  # get "/travels/:id", to: "travels#show"
+  resources :travels
+
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
