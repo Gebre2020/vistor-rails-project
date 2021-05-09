ea2f7fd76fd652151bcf9c4ac25c01160dd97017
Gebremeskel <gebre20122@gmail.com>
2021-05-09 13:39:56 -0500

    creating schema table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_05_07_193157) do
+ActiveRecord::Schema.define(version: 2021_05_08_011921) do
+
+  create_table "locations", force: :cascade do |t|
+    t.string "name"
+    t.string "address"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "travels", force: :cascade do |t|
     t.string "city"
@@ -21,6 +28,14 @@ ActiveRecord::Schema.define(version: 2021_05_07_193157) do
     t.datetime "updated_at", precision: 6, null: false
     t.index ["location_id"], name: "index_travels_on_location_id"
     t.index ["user_id"], name: "index_travels_on_user_id"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "username"
+    t.string "email"
+    t.string "password_digest"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
 end
