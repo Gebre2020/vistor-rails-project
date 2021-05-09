b2dec1ecb12dd5888e8e2316b8428c264c0f6426
Gebremeskel <gebre20122@gmail.com>
2021-05-09 13:38:34 -0500

    signup login logout routes with nested routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,28 @@
 Rails.application.routes.draw do
-  root "travels#index"
+  root 'sessions#home'
+  #root "travels#index"
+  
+  # signup route
+  get '/signup' => 'users#new'
+  post '/signup' => 'users#create'
 
-  # get "/travels", to: "travels#index"
-  # get "/travels/:id", to: "travels#show"
-  resources :travels
+   # login route
+   get '/login' => 'sessions#new'
+   post '/login' => 'sessions#create'
+   get '/welcome' => 'sessions#welcome'
+ 
+   # logout route
+   delete '/logout' => 'sessions#destroy'
 
+  resources :users do
+    resources :travels, only: [:new,:create,:index ]
+  end
+
+  resources :travels do
+    resources :locations, only: [:new,:create,:index ]
+  end
+  resources :locations
+h
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
