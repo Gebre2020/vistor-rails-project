bd7464b2535cf1e24486ce7ac26eff0fe1472ba2
Gebremeskel <gebre20122@gmail.com>
2021-05-09 13:17:19 -0500

    adding helper method

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,28 @@
 class ApplicationController < ActionController::Base
+    # include AplicationHelper
+
+  
+    # gives access to methods in the views
+    # before_action :authorized
+    helper_method :current_user
+    helper_method :logged_in?
+
+    private
+
+    def current_user
+    #   if session[:user_id]
+    #     @current_user = User.find_by(id: session[:user_id])
+    #   end
+       @current_user ||= User.find_by_id(session[:user_id]) if session[:user_id]
+    end
+     
+    def logged_in?
+      !!session[:user_id]
+    end
+  
+    def redirect_if_not_logged_in
+      redirect_to '/' if !logged_in?
+     # redirect_to sign_in_path, alert: "You must be signed in to do that." if current_user.nil?
+    end
+  
 end
