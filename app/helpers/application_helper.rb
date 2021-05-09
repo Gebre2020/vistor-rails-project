aa976bb7f8b6b355c4424e78ba3bf666188010c8
Gebremeskel <gebre20122@gmail.com>
2021-05-09 13:27:26 -0500

    module helper setup and commented code

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,13 @@
 module ApplicationHelper
+    
+    # def current_user
+    #     #   if session[:user_id]
+    #     #     @current_user = User.find_by(id: session[:user_id])
+    #     #   end
+    #     @current_user ||= User.find_by_id(session[:user_id]) if session[:user_id]
+    # end
+         
+    # def logged_in?
+    #     !!session[:user_id]
+    # end
 end
