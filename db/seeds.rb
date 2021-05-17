e657a497f02e74a4474dd579c98fd785cad341a2
Gebremeskel <gebre20122@gmail.com>
2021-05-17 15:37:34 -0500

    create a data to seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,15 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+# user1 = User.create(username: "jhon", email: "jhon@jhon.com", password: "jhon")
+# user2 = User.create(username: "sara", email: "sara@sara.com", password: "sara")
+
+# location1 = Location.create(city: "Chicago", country: "IL, USA")
+# location2 = Location.create(city: "Milwaukee", country: "WI, USA")
+
+# travel1 = Travel.create(name: "Willis Tower", address: "233 S. Wacker Dr", user_id: 1, location_id: 1)
+# travel2 = Travel.create(name: "Field Museum", address: "1400 S. Lakeshore Dr", user_id: 2, location_id: 1)
+# travel3 = Travel.create(name: "Van Gogh", address: "400 W. Wisconsin Ave", user_id: 1, location_id: 2)
+# travel4 = Travel.create(name: "Wild Waterdome Water Park", address: "511 E. Adams St, Wisconsin Dell, WI", user_id: 2, location_id: 2)
+
