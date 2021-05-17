2efe9c8510e5a713770e26719fde5c3b00996efb
Gebremeskel <gebre20122@gmail.com>
2021-05-17 15:10:50 -0500

    validates

--- app/models/location.rb
+++ app/models/location.rb
@@ -1,8 +1,8 @@
 class Location < ApplicationRecord
     has_many :travels
     has_many :users, through: :travels
-
-    accepts_nested_attributes_for :travels, :users
-    validates :name, presence: true
-    validates :address, presence: true
+    
+    validates :city, :country, presence: true
+    accepts_nested_attributes_for :travels
+    
 end
