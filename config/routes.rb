c19b65d3e7b79f24478b9606c187defe474b9bfb
Gebremeskel <gebre20122@gmail.com>
2021-05-17 15:36:10 -0500

    update the routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
   root 'sessions#home'
-  #root "travels#index"
   
   # signup route
   get '/signup' => 'users#new'
@@ -9,20 +8,27 @@ Rails.application.routes.draw do
    # login route
    get '/login' => 'sessions#new'
    post '/login' => 'sessions#create'
-   get '/welcome' => 'sessions#welcome'
+   #get '/welcome' => 'sessions#welcome'
  
    # logout route
    delete '/logout' => 'sessions#destroy'
+   get '/auth/:provider/callback', to: 'sessions#omniauth'
+  
+   resources :users, only: [:new, :create]
+   
+  # nested routes should go in one direction
+  # parent to child
+  # resources :locations do  # 7 restful routes
+  #   resources :travels, only: [:index, :new, :create] #7 restful routes for nested resources
+  # end
+  # resources :travels
+  resources :travels, only: [:index, :new, :create] # anything non-nested
 
-  resources :users do
-    resources :travels, only: [:new,:create,:index ]
+  resources :locations do  # 7 restful routes
+    resources :travels  #, shallow: true #7 restful routes for nested resources
   end
 
-  resources :travels do
-    resources :locations, only: [:new,:create,:index ]
-  end
-  resources :locations
-h
+
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
