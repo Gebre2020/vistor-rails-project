0af5858769442bf02fe25c2f1c65a95997561037
Gebremeskel <gebre20122@gmail.com>
2021-05-17 15:11:40 -0500

    delete nested attributes

--- app/models/travel.rb
+++ app/models/travel.rb
@@ -1,6 +1,7 @@
 class Travel < ApplicationRecord
     belongs_to :user
-    belongs_to :location
-    validates :city, presence: true
-    validates :country, presence: true
+    belongs_to :location, optional: true
+    validates :name, presence: true
+    validates :address, presence: true
+   # accepts_nested_attributes_for :locations
 end
