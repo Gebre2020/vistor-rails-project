461628ce4a0c990a5d90e939a92baf851ae045e4
Gebremeskel <gebre20122@gmail.com>
2021-05-17 15:07:08 -0500

    adding omniauth code

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,29 +5,48 @@ class SessionsController < ApplicationController
   
   def destroy
     if !session[:user_id].nil?
+      # flash[:message] = "Are you sure?" 
       session.delete(:user_id)
       @current_user = nil
     else
-       redirect_to '/'
+       redirect_to "/login"
     end        
   end
 
-  # def new
-  # end
-
+  def new
+    if logged_in?
+      redirect_to root_path
+    end 
+  end
+  
   def create
     user = User.find_by(username: params[:user][:username])
     # if !user
     #   redirect_to '/signup'
     if user && user.authenticate(params[:user][:password])
-        # logging in
-        session[:user_id] = user.id
-        redirect_to user_path(user)
-        # redirect_to "/users/#{user.id}"
+      # logging in
+      session[:user_id] = user.id
+      redirect_to root_path
+      # redirect_to "/users/#{user.id}"
     else
-        flash[:message] = "Invalid email/password combination"
-        redirect_to "/login"
+      flash[:message] = "Invalid email/password combination"
+      redirect_to "/login"
     end
   end
+
+   
+  def omniauth 
+    user = User.find_or_create_by(uid: request.env['omniauth.auth'][:uid], provider: request.env['omniauth.auth'][:provider]) do |u|
+      u.username = request.env['omniauth.auth'][:info][:first_name]
+      u.email = request.env['omniauth.auth'][:info][:email]
+      u.password = SecureRandom.hex(15)
+    end 
+    if user.valid?
+      session[:user_id] = user.id # log them 
+      redirect_to root_path
+    else
+      redirect_to login_path 
+    end 
+  end 
 
 end
