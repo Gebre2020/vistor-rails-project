265ca8a52c60946c0fc7cab475b91c25959222c7
Gebremeskel <gebre20122@gmail.com>
2021-05-17 15:38:45 -0500

    update the attributes

--- db/migrate/20210508003737_create_locations.rb
+++ db/migrate/20210508003737_create_locations.rb
@@ -1,8 +1,8 @@
 class CreateLocations < ActiveRecord::Migration[6.1]
   def change
     create_table :locations do |t|
-      t.string :name
-      t.string :address
+      t.string :city
+      t.string :country
 
       t.timestamps
     end
