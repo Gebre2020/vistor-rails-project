aa9994663403dc0cd8fd9ea9b2bbf63a30c5b95e
Gebremeskel <gebre20122@gmail.com>
2021-05-18 12:51:50 -0500

    adding optional true for user

--- app/models/travel.rb
+++ app/models/travel.rb
@@ -1,5 +1,5 @@
 class Travel < ApplicationRecord
-    belongs_to :user
+    belongs_to :user, optional: true
     belongs_to :location, optional: true
     validates :name, presence: true
     validates :address, presence: true
