32997b39efc93d630b5c22db09b02d3ffc81b29c
Gebremeskel <gebre20122@gmail.com>
2021-05-20 09:18:33 -0500

    writing scope code

--- app/models/travel.rb
+++ app/models/travel.rb
@@ -3,5 +3,12 @@ class Travel < ApplicationRecord
     belongs_to :location, optional: true
     validates :name, presence: true
     validates :address, presence: true
-   # accepts_nested_attributes_for :locations
+    # accepts_nested_attributes_for :locations
+
+    # scope methods 
+    # class methods that query the table associated with model return 
+
+    # return the last 3 travels that were made
+    scope :latest_travels, -> {order(created_at: :desc).limit(3)}
+
 end
