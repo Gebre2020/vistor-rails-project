066f15ee8e364da97d570164d8c568c321a186e9
Gebremeskel <gebre20122@gmail.com>
2021-05-20 09:17:31 -0500

    adding validate email

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,16 @@ class User < ApplicationRecord
   
   validates :username, presence: true
   validates :email, uniqueness: true, presence: true
-  validates :password, presence: true
+  validates :password, length: { in: 6..10 }
   #accepts_nested_attributes_for :locations
+  validate :is_email?
+
+  # custom method 
+
+  def is_email?
+    if !email.match(/\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i)
+      errors.add(:email, "Give me a real email dammit")
+    end 
+  end 
+
 end
