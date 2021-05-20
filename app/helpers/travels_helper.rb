092e676b989b435a44ce28fe7f8a908da8923d1c
Gebremeskel <gebre20122@gmail.com>
2021-05-20 09:19:47 -0500

    commented helper method

--- app/helpers/travels_helper.rb
+++ app/helpers/travels_helper.rb
@@ -1,2 +1,14 @@
 module TravelsHelper
+ 
+    # # pure ruby land
+    # # content_tag for html
+    # # interpolation #{}
+
+    # def show_links
+    #     if @location 
+    #        link_to "New Location Travel", new_location_travel_path 
+    #     else 
+    #        link_to "New Travel", new_travel_path 
+    #     end  
+    # end
 end
