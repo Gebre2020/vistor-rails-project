62d5b14d45c80309b949272231a23691c8097bec
Gebremeskel <gebre20122@gmail.com>
2021-05-20 09:22:28 -0500

    writing the scope methods

--- app/models/location.rb
+++ app/models/location.rb
@@ -5,4 +5,20 @@ class Location < ApplicationRecord
     validates :city, :country, presence: true
     accepts_nested_attributes_for :travels
     
+    # scope method that locations travels by their location
+    
+    # write a scope method as class method
+    
+    def self.locs_travels
+        joins(:travels).group('locations.city')
+    end
+
+    # scope method
+    # scope :locs_travels, -> {joins(:travels).group('locations.city')}
+
+    #alphabetize the list of locations
+    scope :alpha, -> {'city'}
+
+    # show locations with travels more than 2 
+    scope :baller_travels, -> {joins(:travels).where('travels.name > 2').group('locations.city')}
 end
